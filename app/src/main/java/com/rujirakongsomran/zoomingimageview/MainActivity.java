f28abe41a32441280cd80f65b0067e00c817766b
Rujira Kongsomran <biwberry@outlook.com>
2020-05-23 20:06:36 +0700

    finish

--- app/src/main/java/com/rujirakongsomran/zoomingimageview/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/zoomingimageview/MainActivity.java
@@ -3,12 +3,24 @@ package com.rujirakongsomran.zoomingimageview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView ibPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        ibPicture = (ImageView) findViewById(R.id.ibPicture);
+        PhotoViewAttacher photoViewAttacher = new PhotoViewAttacher(ibPicture);
+        photoViewAttacher.update();
     }
 }
